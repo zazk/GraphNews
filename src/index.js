3cc4c5ae32d37ccde3345a4fc9e6da21f42ba62c
Enrique Juan de Dios <juandedioz@gmail.com>
2019-07-13 10:30:37 +0800

    [Setup] Prisma Setting, replace models

--- src/index.js
+++ src/index.js
@@ -1,38 +1,23 @@
 const { GraphQLServer } = require("graphql-yoga");
-
-let links = [
-  {
-    id: "link-9",
-    url: "https://google.com",
-    description: "Yeah the url itself"
-  },
-  {
-    id: "link-0",
-    url: "https://twitter.com/zazk",
-    description: "Another URL from zazk"
-  }
-];
-
-let idCount = links.length;
+const { prisma } = require("./generated/prisma-client");
 
 const resolvers = {
   Query: {
     info: () => "This a tryout shit",
-    feed: () => links,
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    },
     link: (_, args) => {
       const [selected] = links.filter(obj => args.id == obj.id);
       return selected;
     }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
+    post: (_, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
     },
     updateLink: (parent, args) => {
       const [link] = links.filter(obj => args.id == obj.id);
@@ -50,7 +35,8 @@ const resolvers = {
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 server.start(() =>
   console.log("This thing really runs! Is running on http://localhost:4000")
